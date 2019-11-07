2a4ef8896e37a24fb9793de7ea48e452340e00e5
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-07 17:57:27 -0300

    Criando Home Feature

--- BaseProject/BaseProject/Features/Home/View/HomeViewController.swift
+++ BaseProject/BaseProject/Features/Home/View/HomeViewController.swift
@@ -8,23 +8,11 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, Identifiable {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    static func instantiate() -> HomeViewController {
+        let viewController: HomeViewController = UINib.instantiateViewController(from: .home)
+        return viewController
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
