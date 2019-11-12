4024d2a937b09c7d9af3b02ca3d06d483b3d4689
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-12 11:26:51 -0300

    Atualizando UIView+Extensions

--- BaseProject/BaseProject/Extensions/UIView+Extensions.swift
+++ BaseProject/BaseProject/Extensions/UIView+Extensions.swift
@@ -264,6 +264,42 @@ extension UIView {
         gradient.colors = colors
         self.layer.insertSublayer(gradient, at: 0)
     }
+
+    func anchor (top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat = 0.0, paddingLeft: CGFloat = 0.0, paddingBottom: CGFloat = 0.0, paddingRight: CGFloat = 0.0, width: CGFloat, height: CGFloat, enableInsets: Bool = false) {
+        var topInset = CGFloat(0)
+        var bottomInset = CGFloat(0)
+
+        if #available(iOS 11, *), enableInsets {
+            let insets = self.safeAreaInsets
+            topInset = insets.top
+            bottomInset = insets.bottom
+
+            print("Top: \(topInset)”")
+            print("bottom: \(bottomInset)")
+        }
+
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+    }
 }
 
 
