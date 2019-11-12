4f25c94cf0f7d3dabf9074976b9a48322dd05aff
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-12 11:26:40 -0300

    Atualizando Global+Extensions

--- BaseProject/BaseProject/Extensions/Global+Extensions.swift
+++ BaseProject/BaseProject/Extensions/Global+Extensions.swift
@@ -59,3 +59,81 @@ extension UISearchBar {
     }
 }
 
+
+public extension UITableView {
+    func register(_ cell: UITableViewCell.Type) {
+        let nib = UINib(nibName: cell.identifier, bundle: nil)
+        register(nib, forCellReuseIdentifier: cell.identifier)
+    }
+
+    func register(nibName: String, identifier: String? = nil) {
+        let nib = UINib(nibName: nibName, bundle: nil)
+        if let identifier = identifier {
+            register(nib, forCellReuseIdentifier: identifier)
+        } else {
+            register(nib, forCellReuseIdentifier: nibName)
+        }
+    }
+
+    func registerHeaderFooter(_ reusableView: UITableViewHeaderFooterView.Type) {
+        let nib = UINib(nibName: reusableView.identifier, bundle: nil)
+
+        register(nib, forHeaderFooterViewReuseIdentifier: reusableView.identifier)
+    }
+
+    func dequeueReusableCell<T: UITableViewCell>(of class: T.Type,
+                                                 for indexPath: IndexPath,
+                                                 configure: @escaping ((T) -> Void) = { _ in }) -> UITableViewCell {
+        let cell = dequeueReusableCell(withIdentifier: T.identifier, for: indexPath)
+        if let typedCell = cell as? T {
+            configure(typedCell)
+        }
+        return cell
+    }
+
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(of class: T.Type) -> T? {
+        let view = dequeueReusableHeaderFooterView(withIdentifier: T.identifier)
+        if let typedView = view as? T {
+            return typedView
+        }
+        return nil
+    }
+
+    func genericDequeueReusableCell(of identifier: String,
+                                    for indexPath: IndexPath,
+                                    configure: @escaping ((UITableViewCell) -> Void) = { _ in }) -> UITableViewCell {
+        let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        configure(cell)
+        return cell
+    }
+
+    func setHeightTableHeaderView(height: CGFloat) {
+        DispatchQueue.main.async { [weak self] in
+            guard let headerView = self?.tableHeaderView else { return }
+            var newFrame = headerView.frame
+            newFrame.size.height = height
+
+            UIView.animate(withDuration: 0.1) {
+                self?.beginUpdates()
+                headerView.frame = newFrame
+                self?.endUpdates()
+            }
+        }
+    }
+
+    func setHeightTableFooterView(height: CGFloat) {
+        DispatchQueue.main.async { [weak self] in
+            guard let footerView = self?.tableFooterView else { return }
+            var newFrame = footerView.frame
+            newFrame.size.height = height
+
+            UIView.animate(withDuration: 0.1) {
+                self?.beginUpdates()
+                footerView.frame = newFrame
+                self?.endUpdates()
+            }
+        }
+    }
+}
+
+
