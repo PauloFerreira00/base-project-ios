dcc03ba3ae8efc51119ef562943e76d7c8eeb445
Paulo Ferreira <paulo.jesus@aluno.faculdadeimpacta.com.br>
2019-11-12 11:26:04 -0300

    Criando tela de detalhes

---
+++ BaseProject/BaseProject/Features/Home/View/Detail/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  BaseProject
+//
+//  Created by Paulo Ferreira de Jesus - PFR on 08/11/19.
+//  Copyright © 2019 Iteris Consultoria. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+
+}
